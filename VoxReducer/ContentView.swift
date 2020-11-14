dba98476687badca9960615e8d0eb9e3b8b08814
Bill S <bill.e.seaman@gmail.com>
2020-11-13 17:25:54 -0800

    Initial Commit

---
+++ VoxReducer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VoxReducer
+//
+//  Created by Loud on 11/13/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
